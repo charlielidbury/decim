a78c4cc0c39825d18ea871730656a06938bb0f1d
arcayn <12099355+arcayn@users.noreply.github.com>
2021-03-27 19:44:58 +0000

    Frontend stuff

--- ui/App.svelte
+++ ui/App.svelte
@@ -1,17 +1,70 @@
 <script>
 	export let name;
+	import TransactionCard from "./components/TransactionCard.svelte";
+
+	$: transactions = [
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+		{"name":"Burger King", "cost":3, "carbon":0.4},
+	];
+
+	function addTrans() {
+		transactions = [...transactions,{"name":"KFC", "cost":3, "carbon":0.4}];
+		console.log("H");
+	}
 </script>
+
 <div id="mainContainer">
+	
 	<div id="sidebar">
 		<h1 id="decimName">decim<span class="emphasis">.</span>io</h1>
 	</div>
-	
+	<div id="contentContainer">
+		<div class="row headline">
+			<div class="headStat col-md-4">
+
+			</div>
+			<div class="col-md-4">
+				
+			</div>
+			<div class="headStat col-md-4">
+				
+			</div>
+		</div>
+		<div class="transactionList">
+			<h1 class="transTag">Transactions</h1>
+			<button on:click={addTrans}></button>
+		{#each transactions as transaction}
+			<TransactionCard {...transaction}/>
+		{/each}
+		</div>
+	</div>
 </div>
 
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&family=Inconsolata:wght@300&display=swap');
-	
 
+	.headline {
+		height: 30vh;
+		border-bottom: 1px solid white;
+	}
+	.transactionList {
+		max-height: 70vh;
+		overflow-y: auto;
+	}
+	.transTag {
+		font-family: "roc-grotesk-wide", monospace;
+		letter-spacing: 0.1em;
+		padding: 0.5em 1.5em 0 1.5em;
+		color: white;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
@@ -23,15 +76,26 @@
 		background-image: linear-gradient(10deg, rgb(35,35,70), rgb(38,27,56));
 		margin: none;
 		min-height: 100vh;
-		width: 100vw;
+		width: 100%;
+		overflow-x: hidden;
+	}
+
+	#contentContainer {
+		width: 80%;
+		float: right;
+		margin: auto;
 	}
 
 	#sidebar {
-		width: 16vw;
 		height: 100vh;
 		position: fixed;
+		float: left;
+		width: 20%;
 		border-right: 1px solid white;
 		background-image: linear-gradient(100deg, rgb(35,35,70), rgb(38,27,56));
+		padding: 0;
+
+		z-index: 10000;
 	}
 
 	#decimName {
@@ -44,6 +108,7 @@
 		text-align: center;
 		font-weight: 300;
 		font-variant: normal;
+		padding: 0.6em 0 0.6em 0;
 	}
 
 	.emphasis {
---
+++ ui/components/TransactionCard.svelte
@@ -0,0 +1,56 @@
+<script>
+    export let name = "KFC", cost = 0, carbon = 0;
+</script>
+
+<div class="transactionCard row">
+    <p class="col-md-7 transText transName">{name}</p>
+    <div class="colSpacer col-md-1"></div>
+    <p class="col-md-2 transText transCarb">{carbon}kg</p>
+    <p class="col-md-2 transText transOff">£{cost}</p>
+</div>
+
+<style>
+    .transactionCard {
+		background-image: linear-gradient(100deg, rgb(35,35,70), rgb(38,27,56));
+		width: 50vw;
+		
+		margin: auto;
+		margin-top: 2em;
+
+		border: 1px solid white;
+		cursor:pointer;
+		transition: ease transform 0.3s,
+			ease box-shadow 0.3s,
+			ease border-radius 0.3s;
+		border-radius: 2px;
+	}
+
+	.transactionCard:hover {
+		transform: translate(-0.3em,-0.3em);
+		-webkit-box-shadow: 11px 10px 31px -4px rgba(0,0,0,0.49);
+		-moz-box-shadow: 11px 10px 31px -4px rgba(0,0,0,0.49);
+		box-shadow: 11px 10px 31px -4px rgba(0,0,0,0.49);
+		border-radius: 7px;
+	}
+
+    .transText {
+        font-family: "Fira Code", monospace;
+		font-weight: 300;
+		font-size: 1.4em;
+		margin:0;
+		padding: 0.5em;
+    }
+    .transName {
+		color:rgba(255,255,255,0.9);
+	}
+
+	.transCarb {
+		color:greenyellow;
+		text-align: center;
+	}
+
+	.transOff {
+		color:indianred;
+		text-align: center;
+	}
+</style>
