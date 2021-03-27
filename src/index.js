9c5de9799d2c99b8a8924b411a8d1b0ceba2b5c3
Charlie <charlie.lidbury@icloud.com>
2021-03-27 17:22:59 +0000

    ping pong

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+// const cors = require('cors');
+const path = require('path');
 
+// app.use(cors());
 
+app.use(express.static('public'));
 
-console.log("You have run the backend!");
+app.get("/api/ping", (req, res) => {
+    res.status(200);
+    res.json("pong");
+});
+
+app.listen(port, () => {
+    console.log(`Server is up at port ${port}`);
+});
